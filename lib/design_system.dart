fec80bde69b2a4c44d906f26579f354a00e038d5
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-08-22 13:13:25 -0300

    [feat] default button

---
+++ lib/design_system.dart
@@ -0,0 +1,1 @@
+export 'widgets/default_button.dart';
---
+++ lib/widgets/default_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key? key,
+    required this.onPressed,
+    required this.text,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
